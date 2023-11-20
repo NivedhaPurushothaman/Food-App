a6dec402e161eb615c1199dec42b9ab1b0cd66b1
Nivetha Nivi <nivethanivi0331@gmail.com>
2023-11-21 01:57:16 +0530

    Submit changed to Login(Button txt)

--- Web-App/src/components/Login/Login.jsx
+++ Web-App/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 
 function Login() {
 
@@ -12,7 +12,6 @@ function Login() {
         e.preventDefault();
         console.log(userInfo)
     }
-
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3)', padding: '20px', borderRadius: '8px', maxWidth: '300px', margin: 'auto' }}>
@@ -21,7 +20,7 @@ function Login() {
                     <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="text" id='username' onChange={formUserInfo} value={userInfo.username} />
                     <label style={{ display: 'block', marginBottom: '6px', fontWeight: 'bold' }} htmlFor="">Password</label>
                     <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="password" id='password' onChange={formUserInfo} value={userInfo.password} />
-                    <input type="submit" style={{ width: '100%', padding: '8px', boxSizing: 'border-box', backgroundColor: 'blue', color: 'white', borderRadius: '4px', cursor: 'pointer' }} value="Submit" onClick={onSubmit} />
+                    <input type="submit" style={{ width: '100%', padding: '8px', boxSizing: 'border-box', backgroundColor: 'blue', color: 'white', borderRadius: '4px', cursor: 'pointer' }} value="Login" onClick={onSubmit} />
                 </form>
             </div>
         </div>
