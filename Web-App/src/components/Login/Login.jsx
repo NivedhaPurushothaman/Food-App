c83651dfb392f2b17f6afa363e55fad9a84d33e1
Nivetha Nivi <nivethanivi0331@gmail.com>
2023-11-21 01:53:22 +0530

    button style and card position to centre of page

--- Web-App/src/components/Login/Login.jsx
+++ Web-App/src/components/Login/Login.jsx
@@ -14,14 +14,16 @@ function Login() {
     }
 
     return (
-        <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3)', padding: '20px', borderRadius: '8px', maxWidth: '300px', margin: 'auto' }}>
-            <form action="">
-                <label style={{ display: 'block', marginBottom: '6px', fontWeight: 'bold' }} htmlFor="">Username</label>
-                <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="text" id='username' onChange={formUserInfo} value={userInfo.username} />
-                <label style={{ display: 'block', marginBottom: '6px', fontWeight: 'bold' }} htmlFor="">Password</label>
-                <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="password" id='password' onChange={formUserInfo} value={userInfo.password} />
-                <input type="submit" value="" onClick={onSubmit} />
-            </form>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3)', padding: '20px', borderRadius: '8px', maxWidth: '300px', margin: 'auto' }}>
+                <form action="">
+                    <label style={{ display: 'block', marginBottom: '6px', fontWeight: 'bold' }} htmlFor="">Username</label>
+                    <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="text" id='username' onChange={formUserInfo} value={userInfo.username} />
+                    <label style={{ display: 'block', marginBottom: '6px', fontWeight: 'bold' }} htmlFor="">Password</label>
+                    <input style={{ width: '100%', padding: '8px', marginBottom: '12px', boxSizing: 'border-box' }} type="password" id='password' onChange={formUserInfo} value={userInfo.password} />
+                    <input type="submit" style={{ width: '100%', padding: '8px', boxSizing: 'border-box', backgroundColor: 'blue', color: 'white', borderRadius: '4px', cursor: 'pointer' }} value="Submit" onClick={onSubmit} />
+                </form>
+            </div>
         </div>
     )
 }
